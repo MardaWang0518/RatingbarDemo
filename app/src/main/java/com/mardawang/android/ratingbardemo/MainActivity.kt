00d5b24913e55771af030cdef267bc5db3c548e9
wangy@workai.com.cn <wangy@workai.com.cn>
2018-03-30 11:21:29 +0800

    first commit

---
+++ app/src/main/java/com/mardawang/android/ratingbardemo/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.mardawang.android.ratingbardemo
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        rb1.setOnRatingBarChangeListener( RatingBar.OnRatingBarChangeListener())
+    }
+}
